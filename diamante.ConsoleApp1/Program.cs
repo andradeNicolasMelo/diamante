65ac187633aa5cf773030616b10fb1c049719d50
Andrade <andrade.nicolas.melo@outlook.com>
2025-04-26 20:28:30 -0300

    Adicionar arquivos de projeto.

---
+++ diamante.ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            int valorUsuarioDigitou = ValorUsuarioDigitou(0);
+
+            Console.ReadLine();
+        }
+    }
+
+    static int ValorUsuarioDigitou(int valor)
+    {
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("Digite um valor impar: ");
+            string strValor = Console.ReadLine()!;
+
+            bool sucesso = int.TryParse(strValor, out valor);
+
+            if (!sucesso)
+            {
+                Console.WriteLine("Isso não é um numero impar. Clique ENTER para tentar novamente");
+                Console.ReadLine();
+                continue;
+            }
+
+
+            else
+            {
+                if (valor % 2 == 0)
+                {
+                    Console.WriteLine("Isso não é um valor impar. Digite novamente: ");
+                    Console.ReadLine();
+                }
+            }
+        } while (valor % 2 == 0);
+        return valor;
+    }
+}
